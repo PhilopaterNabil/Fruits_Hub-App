74876923a29280a8331c04e376f3a646d8ed209a
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-22 17:35:51 +0300

    Add localization support for Arabic and English
    - Integrated Flutter's localization package and set up localization delegates in FruitsHubApp.
    - Created generated localization files for Arabic and English languages.
    - Added ARB files for localization strings.
    - Updated pubspec.yaml to include flutter_localizations dependency.
    - Generated necessary localization code using the Flutter Intl plugin.

--- lib/fruits_hub_app.dart
+++ lib/fruits_hub_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/core/routing/app_router.dart';
 import 'package:fruits_hub/core/routing/routes.dart';
+import 'package:fruits_hub/generated/l10n.dart';
 
 class FruitsHubApp extends StatelessWidget {
   const FruitsHubApp({super.key});
@@ -8,7 +10,16 @@ class FruitsHubApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Fruits Hub',
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
