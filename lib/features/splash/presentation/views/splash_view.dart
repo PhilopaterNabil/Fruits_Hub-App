f64728758ff0926a80c8c024de45cc99194bb4e5
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-25 21:36:39 +0300

    feat: Refactor SplashView and SplashViewBody for improved navigation and animation

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/helper/extensions.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/managers/splash_cubit/splash_cubit.dart';
 import 'package:fruits_hub/features/splash/presentation/views/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
@@ -6,8 +9,19 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SplashViewBody(),
+    return BlocListener<SplashCubit, SplashState>(
+      listenWhen: (previous, current) => current is SplashNavigateTo,
+      listener: _handleSplashStateListener,
+      child: const Scaffold(
+        body: SplashViewBody(),
+      ),
+    );
+  }
+
+  void _handleSplashStateListener(BuildContext context, SplashState state) {
+    state.maybeWhen(
+      navigateTo: (route) => context.goToNamed(route),
+      orElse: () {},
     );
   }
 }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:fruits_hub/core/routing/routes.dart';
-import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/core/utils/app_images_assets.dart';
-import 'package:fruits_hub/core/utils/constants.dart';
 
-class SplashViewBody extends StatefulWidget {
+class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
-
-  @override
-  State<SplashViewBody> createState() => _SplashViewBodyState();
-}
-
-class _SplashViewBodyState extends State<SplashViewBody> {
-  @override
-  void initState() {
-    excuteNavigation();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,23 +15,17 @@ class _SplashViewBodyState extends State<SplashViewBody> {
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            SvgPicture.asset(AppImagesAssets.imagesPlant),
+            SvgPicture.asset(AppImagesAssets.imagesPlant)
+                .animate()
+                .slideY(begin: -1, end: 0, duration: 300.ms),
           ],
         ),
         SvgPicture.asset(AppImagesAssets.imagesLogo),
         SvgPicture.asset(
           AppImagesAssets.imagesSplashBottom,
           fit: BoxFit.fill,
-        ),
+        ).animate().slideY(begin: 1, end: 0, duration: 300.ms),
       ],
     );
-  }
-
-  void excuteNavigation() {
-    bool isOnBoardingViewSeen = SharedPreferencesSingleton.getBool(kIsOnBoardingViewSeen);
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(
-          context, isOnBoardingViewSeen ? Routes.loginScreen : Routes.onBoardingScreen);
-    });
   }
 }
