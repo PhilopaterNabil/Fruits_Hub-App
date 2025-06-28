a439fe3cd7500fde2326d14c939bd793db94b2e8
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-28 11:49:25 +0300

    feat: Implement Firebase authentication service with user creation and error handling

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/core/errors/exceptions.dart';
+
+class FirebaseAuthService {
+  FirebaseAuthService._();
+  static final instance = FirebaseAuthService._();
+
+  Future<User> createUserWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException(message: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(message: 'The account already exists for that email.');
+      } else {
+        throw CustomException(
+            message: 'An error occurred while creating the user. Please try again later.');
+      }
+    } catch (e) {
+      throw CustomException(
+          message: 'An error occurred while creating the user. Please try again later.');
+    }
+  }
+}
