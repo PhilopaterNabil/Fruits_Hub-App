99bef788e79a93ee764b30e69692384ae346b6e4
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-28 11:09:39 +0300

    feat: Integrate Firebase into the project with necessary configurations and options

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:fruits_hub/core/di/dependency_injection.dart';
 import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
+import 'package:fruits_hub/firebase_options.dart';
 import 'package:fruits_hub/fruits_hub_app.dart';
 
 void main() async {
@@ -9,5 +11,8 @@ void main() async {
   FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
   await SharedPreferencesSingleton.init();
   await setupGetIt();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FruitsHubApp());
 }
