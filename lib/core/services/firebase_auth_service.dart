18f0ea91acb1d81f44727e917be120dfa231f442
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-29 19:42:09 +0300

    feat: Enhance error logging in FirebaseAuthService and AuthRepoImpl for better debugging

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fruits_hub/core/errors/exceptions.dart';
 
@@ -11,14 +13,18 @@ class FirebaseAuthService {
       );
       return credential.user!;
     } on FirebaseAuthException catch (e) {
+      log('Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()} and code: ${e.code}');
       if (e.code == 'weak-password') {
         throw CustomException(message: 'كلمة المرور ضعيفة ، يجب ان تحتوي على 6 حروف على الاقل');
       } else if (e.code == 'email-already-in-use') {
         throw CustomException(message: 'لقد تم انشاء حساب بالفعل بهذا البريد الالكتروني');
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(message: 'لا يوجد اتصال بالانترنت ، يرجى المحاولة مرة اخرى');
       } else {
         throw CustomException(message: 'لقد حدث خطأ ما، يرجى المحاولة مرة اخرى');
       }
     } catch (e) {
+      log('Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()}');
       throw CustomException(message: 'لقد حدث خطأ ما ، يرجى المحاولة مرة اخرى');
     }
   }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
 import 'package:fruits_hub/core/errors/exceptions.dart';
 import 'package:fruits_hub/core/errors/failures.dart';
@@ -23,6 +25,7 @@ class AuthRepoImpl extends AuthRepo {
     } on CustomException catch (e) {
       return left(ServerFailure(e.message));
     } catch (e) {
+      log('Exception in AuthRepoImpl.createUserWithEmailAndPassword: ${e.toString()}');
       return left(ServerFailure('لقد حدث خطأ ما ، يرجى المحاولة مرة اخرى'));
     }
   }
