82e8e1d69aed729ffb886aa25875ecff720667bd
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-30 23:25:13 +0300

    feat: Refactor LoginButtonBlocConsumer to accept onPressed callback and enhance login functionality in LoginViewBody

--- lib/features/auth/presentation/views/widgets/login_button_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/login_button_bloc_consumer.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/helper/build_error_bar.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_progress_hud.dart';
 import 'package:fruits_hub/features/auth/presentation/managers/login_cubit/login_cubit.dart';
 
 class LoginButtonBlocConsumer extends StatelessWidget {
-  const LoginButtonBlocConsumer({super.key});
+  const LoginButtonBlocConsumer({super.key, required this.onPressed});
+
+  final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<LoginCubit, LoginState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        state.maybeWhen(
+          success: (user) => print(user),
+          failure: (message) => buildErrorBar(context, message),
+          orElse: () {},
+        );
+      },
       builder: (context, state) {
         return CustomProgressHud(
           isLoading: state is LoginLoading,
           child: CustomButton(
             title: 'تسجيل دخول',
-            onPressed: () {},
+            onPressed: onPressed,
           ),
         );
       },
--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,77 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/helper/extensions.dart';
 import 'package:fruits_hub/core/routing/routes.dart';
-import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_images_assets.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/core/widgets/password_field.dart';
+import 'package:fruits_hub/features/auth/presentation/managers/login_cubit/login_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/auth_switch_prompt.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/login_button_bloc_consumer.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/or_divider.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/social_login_button.dart';
 
-class LoginViewBody extends StatelessWidget {
+class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key});
+
+  @override
+  State<LoginViewBody> createState() => _LoginViewBodyState();
+}
+
+class _LoginViewBodyState extends State<LoginViewBody> {
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? email, password;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            SizedBox(height: 24),
-            CustomTextFormField(
-              hintText: 'البريد الإلكتروني',
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 16),
-            CustomTextFormField(
-              hintText: 'كلمة المرور',
-              keyboardType: TextInputType.visiblePassword,
-              suffixIcon: Icon(
-                Icons.remove_red_eye_outlined,
-                color: AppColors.silverGreyColor,
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              CustomTextFormField(
+                hintText: 'البريد الإلكتروني',
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (value) {
+                  email = value!;
+                },
               ),
-            ),
-            SizedBox(height: 16),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'نسيت كلمة المرور؟',
-                style: AppTextStyles.font13AccentGreenSemiBold,
+              SizedBox(height: 16),
+              PasswordField(
+                onSaved: (value) {
+                  password = value!;
+                },
               ),
-            ),
-            SizedBox(height: 33),
-            LoginButtonBlocConsumer(),
-            SizedBox(height: 26),
-            AuthSwitchPrompt(
-              question: 'لا تمتلك حساب؟ ',
-              actionText: 'قم بإنشاء حساب',
-              onActionTap: () => context.pushReplacementTo(Routes.signupScreen),
-            ),
-            SizedBox(height: 33),
-            OrDivider(),
-            SizedBox(height: 21),
-            SocialLoginButton(
-              onPressed: () {},
-              image: AppImagesAssets.imagesGoogleIcon,
-              title: 'تسجيل بواسطة جوجل',
-            ),
-            SizedBox(height: 16),
-            SocialLoginButton(
-              onPressed: () {},
-              image: AppImagesAssets.imagesApplIcon,
-              title: 'تسجيل بواسطة أبل',
-            ),
-            SizedBox(height: 16),
-            SocialLoginButton(
-              onPressed: () {},
-              image: AppImagesAssets.imagesFacebookIcon,
-              title: 'تسجيل بواسطة فيسبوك',
-            ),
-            SizedBox(height: 8),
-          ],
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'نسيت كلمة المرور؟',
+                  style: AppTextStyles.font13AccentGreenSemiBold,
+                ),
+              ),
+              SizedBox(height: 33),
+              LoginButtonBlocConsumer(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+
+                    context
+                        .read<LoginCubit>()
+                        .signInWithEmailAndPassword(email: email!, password: password!);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              ),
+              SizedBox(height: 26),
+              AuthSwitchPrompt(
+                question: 'لا تمتلك حساب؟ ',
+                actionText: 'قم بإنشاء حساب',
+                onActionTap: () => context.pushReplacementTo(Routes.signupScreen),
+              ),
+              SizedBox(height: 33),
+              OrDivider(),
+              SizedBox(height: 21),
+              SocialLoginButton(
+                onPressed: () {},
+                image: AppImagesAssets.imagesGoogleIcon,
+                title: 'تسجيل بواسطة جوجل',
+              ),
+              SizedBox(height: 16),
+              SocialLoginButton(
+                onPressed: () {},
+                image: AppImagesAssets.imagesApplIcon,
+                title: 'تسجيل بواسطة أبل',
+              ),
+              SizedBox(height: 16),
+              SocialLoginButton(
+                onPressed: () {},
+                image: AppImagesAssets.imagesFacebookIcon,
+                title: 'تسجيل بواسطة فيسبوك',
+              ),
+              SizedBox(height: 8),
+            ],
+          ),
         ),
       ),
     );
