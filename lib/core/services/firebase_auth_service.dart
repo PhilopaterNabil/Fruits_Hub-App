7a835783d5a1ecb81c9afbbfbc92162889b3e18b
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-30 23:46:13 +0300

    feat: Add Google Sign-In functionality and update dependencies for authentication

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fruits_hub/core/errors/exceptions.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseAuthService {
   Future<User> createUserWithEmailAndPassword(
@@ -49,5 +50,18 @@ class FirebaseAuthService {
       log('Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()}');
       throw CustomException(message: 'لقد حدث خطأ ما ، يرجى المحاولة مرة اخرى');
     }
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
