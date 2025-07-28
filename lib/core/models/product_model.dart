83691473b812bec026919411d0b3ccb7e37fbf20
Philopater Nabil <fylwbatyrnbylnasr@gmail.com>
2025-07-28 20:40:14 +0300

    feat(entities): add ProductEntity and ReviewEntity to represent product and review data structures feat(models): create ProductModel and ReviewModel for data manipulation and conversion feat(repos): implement ProductsRepo interface for product data retrieval functionality

---
+++ lib/core/models/product_model.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:fruits_hub/core/entities/product_entity.dart';
+import 'package:fruits_hub/core/models/review_model.dart';
+
+class ProductModel {
+  final String name;
+  final String code;
+  final String description;
+  final num price;
+  final File image;
+  final bool isFeatured;
+  String? imageUrl;
+  final int expirationsMonths;
+  bool isOrganic;
+  final int numberOfCalories;
+  final num avgRating = 0;
+  final num ratingCount = 0;
+  final int unitAmount;
+  final List<ReviewModel> reviews;
+
+  ProductModel({
+    required this.name,
+    required this.code,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.isFeatured,
+    this.imageUrl,
+    required this.expirationsMonths,
+    this.isOrganic = false,
+    required this.numberOfCalories,
+    required this.unitAmount,
+    required this.reviews,
+  });
+
+  factory ProductModel.fromEntity(ProductEntity entity) {
+    return ProductModel(
+      name: entity.name,
+      code: entity.code,
+      description: entity.description,
+      price: entity.price,
+      image: entity.image,
+      isFeatured: entity.isFeatured,
+      imageUrl: entity.imageUrl,
+      expirationsMonths: entity.expirationsMonths,
+      isOrganic: entity.isOrganic,
+      numberOfCalories: entity.numberOfCalories,
+      unitAmount: entity.unitAmount,
+      reviews: entity.reviews.map((review) => ReviewModel.fromEntity(review)).toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'code': code,
+      'description': description,
+      'price': price,
+      'imageUrl': imageUrl,
+      'isFeatured': isFeatured,
+      'expirationsMonths': expirationsMonths,
+      'isOrganic': isOrganic,
+      'numberOfCalories': numberOfCalories,
+      'unitAmount': unitAmount,
+      'reviews': reviews.map((review) => review.toJson()).toList(),
+    };
+  }
+}
---
+++ lib/core/repos/products_repo/products_repo.dart
@@ -0,0 +1,3 @@
+abstract class ProductsRepo {
+  Future<void> getProducts();
+}
